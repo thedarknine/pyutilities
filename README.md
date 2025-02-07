6ee36a9f11c5123bb97cf8d46c570df5ca52d9dd
TheDarkNine <thedarknine@proton.me>
2025-02-07 12:13:18 +0100

    feat: 🎸 Utilities
    Add display and logs management

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # pyutilities
 Python utilities for terminal display and logs management
+
+## Usage
+
+Lint file before commit
+> poetry run black display.py logs.py --diff
+
+Lint docstring
+> poetry run ruff check
+
+Test
+> poetry run pytest 
---
+++ tests/display_test.py
@@ -0,0 +1,45 @@
+"""Test to check display utilities."""
+
+import arrow
+from display import (
+    info, alert, title, items_list, clear_screen, start_info, end_info
+)
+
+
+def test_info():
+    """Test info."""
+    info("Test info")
+
+
+def test_alert():
+    """Test alert."""
+    alert("Test alert")
+
+
+def test_title():
+    """Test title."""
+    title("Test title")
+
+
+def test_items_list():
+    """Test items list."""
+    items_list(["Test item 1", "Test item 2", "Test item 3"])
+    items_list([])
+    items_list(None)
+    items_list("")
+    items_list(1)
+
+
+def test_clear_screen():
+    """Test clear screen."""
+    clear_screen()
+
+
+def test_start_info():
+    """Test start info."""
+    start_info(arrow.now(), "Test start info")
+
+
+def test_end_info():
+    """Test end info."""
+    end_info(arrow.now())
